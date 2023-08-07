50c256bb83479db61a357cd474fc40cf6c59cf3e
GillesNorce <gipe@norceresearch.no>
2023-08-07 14:35:33 +0200

    Introducing IEquivalent instead of IEquatable

---
+++ OSDC.DotnetLibraries.General/OSDC.DotnetLibraries.General.Math/IEquivalent.cs
@@ -0,0 +1,12 @@
+﻿namespace OSDC.DotnetLibraries.General.Math
+{
+    /// <summary>
+    /// a generic interface for things that are equal with a numerical precision (IEquatable wording is avoided due to conflict with System.IEquatable<T>)
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IEquivalent<T>
+    {
+        bool EQ(T a);
+        bool EQ(T a, double precision);
+    }
+}
