14ccefc8707d5679756c9aae9787955aedaa9e8f
GillesNorce <gipe@norceresearch.no>
2025-11-07 15:49:28 +0100

    Adding bounding box definition (> upgrading to 1.1.1)

---
+++ OSDC.DotnetLibraries.General/OSDC.DotnetLibraries.General.Math/BoundingBox2D.cs
@@ -0,0 +1,24 @@
+﻿namespace OSDC.DotnetLibraries.General.Math
+{
+    public class BoundingBox2D
+    {
+        public double MinX { get; }
+        public double MinY { get; }
+        public double MaxX { get; }
+        public double MaxY { get; }
+
+        public BoundingBox2D(double minX, double minY, double maxX, double maxY)
+        {
+            MinX = minX;
+            MinY = minY;
+            MaxX = maxX;
+            MaxY = maxY;
+        }
+
+        public double LengthX => MaxX - MinX;
+        public double LengthY => MaxY - MinY;
+
+        public bool Contains(double x, double y) =>
+            x >= MinX && x <= MaxX && y >= MinY && y <= MaxY;
+    }
+}
---
+++ OSDC.DotnetLibraries.General/OSDC.DotnetLibraries.General.Math/BoundingBox3D.cs
@@ -0,0 +1,32 @@
+﻿namespace OSDC.DotnetLibraries.General.Math
+{
+    public class BoundingBox3D
+    {
+        public double MinX { get; }
+        public double MinY { get; }
+        public double MinZ { get; }
+        public double MaxX { get; }
+        public double MaxY { get; }
+        public double MaxZ { get; }
+
+        public BoundingBox3D(double minX, double minY, double minZ,
+                             double maxX, double maxY, double maxZ)
+        {
+            MinX = minX;
+            MinY = minY;
+            MinZ = minZ;
+            MaxX = maxX;
+            MaxY = maxY;
+            MaxZ = maxZ;
+        }
+
+        public double LengthX => MaxX - MinX;
+        public double LengthY => MaxY - MinY;
+        public double LengthZ => MaxZ - MinZ;
+
+        public bool Contains(double x, double y, double z) =>
+            x >= MinX && x <= MaxX &&
+            y >= MinY && y <= MaxY &&
+            z >= MinZ && z <= MaxZ;
+    }
+}
