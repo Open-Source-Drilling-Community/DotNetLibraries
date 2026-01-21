cd0cdaa7a8146a96b3f7814456cd285ad312fc30
EricCayeux <eric.cayeux@norceresearch.no>
2026-01-21 19:02:58 +0100

    added MQTTTopicAttribute

---
+++ OSDC.DotnetLibraries.General/OSDC.DotnetLibraries.Drilling.DrillingProperties/MQTTTopicAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSDC.DotnetLibraries.Drilling.DrillingProperties
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class MQTTTopicAttribute : Attribute
+    {
+        public virtual string? Topic { get; set; } = null;
+
+        public MQTTTopicAttribute(string? topic) : base()
+        {
+            Topic = topic;
+        }
+    }
+}
