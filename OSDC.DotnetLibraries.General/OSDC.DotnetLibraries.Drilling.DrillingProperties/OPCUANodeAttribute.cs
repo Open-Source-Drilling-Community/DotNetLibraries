6b62294f0e156da66b505890b33e0dc4ab5c4010
EricCayeux <eric.cayeux@norceresearch.no>
2026-01-26 08:14:40 +0100

    added OPCUANodeAttribute

---
+++ OSDC.DotnetLibraries.General/OSDC.DotnetLibraries.Drilling.DrillingProperties/OPCUANodeAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSDC.DotnetLibraries.Drilling.DrillingProperties
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class OPCUANodeAttribute : Attribute
+    {
+        public virtual string? NameSpace { get; set; } = null;
+        public virtual string? ID { get; set; }
+
+        public OPCUANodeAttribute(string? ns, string? id) : base()
+        {
+            NameSpace = ns;
+            ID = id;
+        }
+    }
+}
